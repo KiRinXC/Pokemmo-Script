1d17f15883fa1612edcaed39c90cc9264207c7c0
KiRinXC <guowenquan24@mails.ucas.ac.cn>
2025-01-04 23:47:04 +0800

    主函数，抓闪光

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ from utili import adjust_window
 from PokeOS import CatchFlash
 
 if __name__ == '__main__':
-    # 共享的标志变量，用于控制线程是否应该继续运行
     window = adjust_window()
     mouse = MouseMove()
     pyautogui.moveTo(mouse.win_p[0]+5, mouse.win_p[1]+5)
     pyautogui.click()
     CF = CatchFlash("闪光")
     CF.run()
-
-
-
-
 
 
 
